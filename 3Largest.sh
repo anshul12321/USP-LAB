ade52ba005d7f61da36f23f0e53566a7bbe61ac4
anshul12321 <54666281+anshul12321@users.noreply.github.com>
2020-10-13 12:58:01 +0300

    Create 3Largest.sh

---
+++ 3Largest.sh
@@ -0,0 +1,15 @@
+#Largest of 3 numbers
+echo "Enter 3 numbers: "
+read a b c
+l=$a
+if test $b -gt $l
+then
+	l=$b
+fi
+
+if test $c -gt $l
+then
+	l=$c
+fi
+
+echo "$l is the largest number amongst $a, $b, and $c."
